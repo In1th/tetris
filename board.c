b2454fb9333d8e38b5a97521e698a3f451c4684b
Mateusz Kruk <mateusz64.kruk@student.uj.edu.pl>
2020-11-25 15:48:57 +0100

    Add block functionality
    Adding block generation functionality for all non-reversed blocks, with the falling logic.

---
+++ board.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+
+#define BOARD_HEIGHT 22
+#define BOARD_WIDTH 14
+
+#define abs(x) x>0? x: -x
+
+//$(pkg-config allegro-5 allegro_font-5 allegro_image-5 --libs --cflags)
+
+/* Structure containing all info about a signle block
+ *
+ * member: pattern[4][4] - 4x4 array containing location of single block pieces in bits {0, 1}
+ * member: r,g,b - contains a value of block's color in RGB
+ * member: x,y - contains x and y coodrinates of the upper-left corner of the block
+ *
+ */
+
+const char oBlock[2][2] = {{1,1},{1,1}};
+const char tBLock[3][3] = {{0,1,0},{1,1,1},{0,0,0}};
+const char lBLock[3][3] = {{0,1,0},{0,1,0},{0,1,1}};
+const char iBlock[4][4] = {{0,0,1,0},{0,0,1,0},{0,0,1,0},{0,0,1,0}};
+const char sBlock[4][4] = {{0,0,0,0},{0,1,1,0},{1,1,0,0},{0,0,0,0}};
+
+struct Block
+{
+    char pattern[4][4];
+    char n;
+    ALLEGRO_COLOR color;
+    int x,y;
+};
+
+//Checks if the block is on the board
+//TODO: make it to work with only the block and not array
+int is_on_board(int x, int y){
+    if ((x > 0) && (BOARD_WIDTH-1 > x) && (y > 0) && (BOARD_HEIGHT-1 > y)) return 1;
+    else return 0;
+}
+
+void reverse(struct Block b){
+    int bufor;
+
+    int i,j;
+
+    for(i = 0; i < 4; i++){
+        for (j = 0; j < 4; j++){
+            bufor = b.pattern[i][abs(j-3)];
+            b.pattern[i][abs(j-3)] = b.pattern[i][j];
+            b.pattern[i][j] = bufor;
+        }
+    }
+}
+
+/* Generates a Block structure from predetermined values
+ *
+ * param: type - type of the block that has to be generated
+ *             { 
+ *               0 - O-block,
+ *               1 - T-block,
+ *               2 - J-block/L-block,
+ *               3 - I-block,
+ *               4 - S-block/Z-block 
+ *             }
+ * param: inverted - a boolean for triggering the inversion of the block 
+ * [NOTE: has to only work with type 3 and 4 blocks]
+ * param: colors[3] - RBG value of the color for the block [default is red]
+ *
+ * returns: fully initialised BLock structure 
+ */
+struct Block generate_block(char type, char reversed, unsigned char colors[3]){
+    struct Block newBlock;
+
+    int i, j;
+
+    if ( (colors[0] >= 0) && (colors[1] >= 0) && (colors[2] >= 0) ){
+        newBlock.color = al_map_rgb(colors[0], colors[1], colors[2]);
+    }
+    else {
+        printf("At least one of the color vaules of the generated block is negative!\n");
+        newBlock.color = al_map_rgb(255,0,0);
+    }
+
+    newBlock.x = 5;
+    newBlock.y = 1;
+    
+
+        switch (type){
+            case 0: {
+                newBlock.n = 2;
+                for (i = 0; i< 4; i++){
+                    for (j = 0;j<4;j++){
+                        if (i < newBlock.n && j < newBlock.n) newBlock.pattern[i][j] = oBlock[i][j];
+                        else newBlock.pattern[i][j] = 0;
+                    }
+                }
+
+            } break;
+            case 1: {
+                newBlock.n = 3;
+                for (i = 0; i< 4; i++){
+                    for (j = 0;j<4;j++){
+                        if (i < newBlock.n && j < newBlock.n) newBlock.pattern[i][j] = tBLock[i][j];
+                        else newBlock.pattern[i][j] = 0;
+                    }
+                }
+            } break;
+            case 2: {
+                newBlock.n = 3;
+                for (i = 0; i< 4; i++){
+                    for (j = 0;j<4;j++){
+                        if (i < newBlock.n && j < newBlock.n) newBlock.pattern[i][j] = lBLock[i][j];
+                        else newBlock.pattern[i][j] = 0;
+                    }
+                }
+            } break;
+            case 3: {
+                newBlock.n = 4;
+                for (i = 0; i< 4; i++){
+                    for (j = 0;j<4;j++){
+                        if (i < newBlock.n && j < newBlock.n)newBlock.pattern[i][j] = iBlock[i][j];
+                        else newBlock.pattern[i][j] = 0;
+                    }
+                }
+            } break;
+            case 4: {
+                newBlock.n = 4;
+                newBlock.y--;
+                for (i = 0; i< 4; i++){
+                    for (j = 0;j<4;j++){
+                        if (i < newBlock.n && j < newBlock.n) newBlock.pattern[i][j] = sBlock[i][j];
+                        else newBlock.pattern[i][j] = 0;
+                    }
+                }
+            } break;
+            default: {
+                printf("Invalid block type!\n");
+                newBlock.n = 0;
+                for (i = 0; i< 4; i++){
+                    for (j = 0;j<4;j++){
+                        newBlock.pattern[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if (reversed == 1){
+            //reverse(newBlock);
+        }
+
+    return newBlock;
+}
+
+
+void must_init(bool test, const char *description){
+    if(test) return;
+
+    printf("couldn't initialize %s\n", description);
+    exit(1);
+}
+
+void draw_board(int x, int y, ALLEGRO_BITMAP* bm){
+
+ int i,j;
+
+ for (i = 0; i < BOARD_HEIGHT; i++){
+    for (j = 0; j < BOARD_WIDTH; j++){
+        if ((BOARD_HEIGHT-1 == i) || (i == 0) || (j == 0) || (BOARD_WIDTH-1 == j) ){
+            al_draw_bitmap(bm, (x + 16*j), (y + 16*i), 0);
+        }
+    }
+ }
+
+}
+
+void draw_block(int x, int y,struct Block block, ALLEGRO_BITMAP* bm){
+    int i,j;
+
+    for (i = 0; i < 4; i++){
+        for (j = 0; j < 4; j++){
+            if (block.pattern[i][j] == 1){
+                al_draw_tinted_bitmap(bm, block.color , (x + 16*block.x +16*j), (y + 16*block.y +16*i), 0);
+            }
+        }
+    }
+}
--- tetris.c
+++ tetris.c
@@ -4,40 +4,35 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
 
+#include "board.c"
+
 #define BOARD_HEIGHT 22
 #define BOARD_WIDTH 14
 
+#define abs(x) x>0? x: -x
+
 //$(pkg-config allegro-5 allegro_font-5 allegro_image-5 --libs --cflags)
 
-void must_init(bool test, const char *description)
-{
-    if(test) return;
+/* Structure containing all info about a signle block
+ *
+ * member: pattern[4][4] - 4x4 array containing location of single block pieces in bits {0, 1}
+ * member: r,g,b - contains a value of block's color in RGB
+ * member: x,y - contains x and y coodrinates of the upper-left corner of the block
+ *
+ */
 
-    printf("couldn't initialize %s\n", description);
-    exit(1);
-}
-
-void drawBoard(int x, int y, ALLEGRO_BITMAP* bm){
-
- int i,j;
-
- for (i = 0; i < BOARD_HEIGHT; i++){
-    for (j = 0; j < BOARD_WIDTH; j++){
-        if ((BOARD_HEIGHT-1 == i) || (i == 0) || (j == 0) || (BOARD_WIDTH-1 == j) ){
-            al_draw_bitmap(bm, (x + 16*j), (y + 16*i), 0);
-        }
-    }
- }
-
-}
 
 int main()
 {
     must_init(al_init(), "allegro");
     must_init(al_install_keyboard(), "keyboard");
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
-    must_init(timer, "timer");
+    ALLEGRO_TIMER* drawingTimer = al_create_timer(1.0 / 30.0);
+    must_init(drawingTimer, "timer");
+    ALLEGRO_TIMER* fallTimer = al_create_timer(1.0);
+    must_init(fallTimer, "timer");
+
+
 
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
     must_init(queue, "queue");
@@ -54,13 +49,21 @@ int main()
 
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_register_event_source(queue, al_get_timer_event_source(fallTimer));
+    al_register_event_source(queue, al_get_timer_event_source(drawingTimer));
 
     bool done = false;
     bool redraw = true;
+    bool fall = false;
     ALLEGRO_EVENT event;
 
-    al_start_timer(timer);
+    al_start_timer(drawingTimer);
+    al_start_timer(fallTimer);
+
+    unsigned char col[3] = {0,0,255};
+    struct Block b = generate_block(4,1,col);
+
+    
     while(1)
     {
         al_wait_for_event(queue, &event);
@@ -69,7 +72,8 @@ int main()
         {
             case ALLEGRO_EVENT_TIMER:
                 // game logic goes here.
-                redraw = true;
+                if (event.timer.source == fallTimer) fall = true;
+                if (event.timer.source == drawingTimer) redraw = true;
                 break;
 
             case ALLEGRO_EVENT_KEY_DOWN:
@@ -81,12 +85,18 @@ int main()
         if(done)
             break;
 
+        if(fall){
+            b.y++;
+            fall = false;
+        }
+
         if(redraw && al_is_event_queue_empty(queue))
         {
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "TETRIS MODAFUKA!");
 
-            drawBoard(100,100, block);
+            draw_board(100,100, block);
+            draw_block(100,100, b, block);
             //al_draw_bitmap(block, 100, 100, 0);
             //al_draw_bitmap(block, 116, 100, 0);
 
@@ -99,8 +109,11 @@ int main()
     al_destroy_bitmap(block);
     al_destroy_font(font);
     al_destroy_display(disp);
-    al_destroy_timer(timer);
+    al_destroy_timer(drawingTimer);
+    al_destroy_timer(fallTimer);
     al_destroy_event_queue(queue);
 
     return 0;
 }
+
+
